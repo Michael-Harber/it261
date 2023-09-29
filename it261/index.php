413c8d42c4bad96f3c42cadb451b534d7407d063
Michael-Harber <129912256+Michael-Harber@users.noreply.github.com>
2023-09-29 10:31:33 -0700

    Add files via upload

---
+++ it261/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portal Page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<h1><a href="index.php">Michael's Portal Page</a></h1>
+<h2>The navigation below represents our BIG assignments.</h2>
+<div id="wrapper">
+
+
+<nav>
+    <ul>
+    <li><a href="">Switch</a></li>
+    <li><a href="">Troubleshoot</a></li>
+    <li><a href="">Calculator</a></li>
+    <li><a href="">Email</a></li>
+    <li><a href="">Database</a></li>
+    <li><a href="">Gallery</a></li>
+    </ul>
+</nav>
+
+<main>
+<h2>About Michael</h2>
+<img class="right" src="images/"  alt="Michael">
+<p>A paragraph about myself, i.e. goals, backgrounds, hobbies, etc. etc.
+Arcu cursus euismod quis viverra nibh cras. Tristique risus nec feugiat in fermentum. Amet cursus sit amet dictum sit amet justo. Porttitor eget dolor morbi non arcu risus quis. Tortor at auctor urna nunc id. At elementum eu facilisis sed odio morbi quis commodo odio. Tincidunt ornare massa eget egestas purus viverra accumsan in. Nibh mauris cursus mattis molestie a iaculis. Fermentum et sollicitudin ac orci phasellus egestas tellus. Pharetra pharetra massa massa ultricies mi quis hendrerit. Dictum sit amet justo donec enim diam. Morbi leo urna molestie at elementum. A arcu cursus vitae congue mauris rhoncus aenean vel. Feugiat in fermentum posuere urna nec tincidunt praesent semper. Risus commodo viverra maecenas accumsan lacus. Scelerisque felis imperdiet proin fermentum leo. Odio pellentesque diam volutpat commodo sed. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Ornare suspendisse sed nisi lacus.    
+</p> 
+</main>
+
+<aside>
+<h2>Weekly Class Exercises</h2> 
+<h3>Week 2</h3>
+<ul>
+    <li><a href="var.php">var.php</li>   
+</ul>   
+</aside>
+
+
+</div>
+
+</body>
+</html>

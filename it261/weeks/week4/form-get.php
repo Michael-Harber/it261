7f3893c58729d86ca0d9c4f32a97e625ac79da75
Michael-Harber <129912256+Michael-Harber@users.noreply.github.com>
2023-10-23 09:57:16 -0700

    Add files via upload

---
+++ it261/weeks/week4/form-get.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_GET['name'],
+        $_GET['email'])) {
+            $name = $_GET['name'];
+            $email = $_GET['email'];
+        } else {
+            
+            
+echo '
+<form action = "" method="">
+<label>NAME</label>
+<input type="text" name="name" >
+<label>EMAIL</label>
+<input type="email" name="email" >
+<input type="submit" vale="Confirm" >
+
+
+</form>
+';
+}

c4e131a30512275fe5935865e8ea3b870a19b7ee
Michael-Harber <129912256+Michael-Harber@users.noreply.github.com>
2023-12-03 13:07:45 -0800

    Add files via upload

---
+++ week9/credentials.php
@@ -0,0 +1,8 @@
+<?php // my credentials that will allow me to connect to the database!!
+
+
+
+define('DB_NAME', 'michaeu7_fall2023');
+define('DB_USER', 'michaeu7_fall2023_user');
+define('DB_PASSWORD', 'phpissuperfun');
+define('DB_HOST', 'localhost');
